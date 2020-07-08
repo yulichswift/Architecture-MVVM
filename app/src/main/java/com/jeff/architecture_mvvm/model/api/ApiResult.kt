28072818b3a282a151e85b30000c0316c14665ce
Jeff.Yu <jeffyu925@gmail.com>
2020-07-08 16:30:50 +0800

    Refactor ApiResult.kt

--- app/src/main/java/com/jeff/architecture_mvvm/model/api/ApiResult.kt
+++ app/src/main/java/com/jeff/architecture_mvvm/model/api/ApiResult.kt
@@ -1,30 +1,30 @@
 package com.jeff.architecture_mvvm.model.api
 
-sealed class ApiResult<T> {
+sealed class ApiResult<out T> {
     companion object {
-        fun <T> loading(): ApiResult<T> {
-            return Loading()
-        }
-
-        fun <T> loaded(): ApiResult<T> {
-            return Loaded()
-        }
-
-        fun <T> error(throwable: Throwable): ApiResult<T> {
-            return Error(throwable)
-        }
-
         fun <T> success(result: T?): ApiResult<T> {
             return when (result) {
-                null -> SuccessNoContent()
+                null -> SuccessNoContent
                 else -> Success(result)
             }
         }
+
+        fun <T> failure(throwable: Throwable): ApiResult<T> {
+            return Failure(throwable)
+        }
+
+        fun <T> loading(): ApiResult<T> {
+            return Loading
+        }
+
+        fun <T> loaded(): ApiResult<T> {
+            return Loaded
+        }
     }
 
-    class Loading<T> : ApiResult<T>()
-    class Loaded<T> : ApiResult<T>()
-    class Error<T>(val exception: Throwable) : ApiResult<T>()
-    class SuccessNoContent<T> : ApiResult<T>()
-    data class Success<T>(val data: T) : ApiResult<T>()
+    data class Success<out T>(val data: T) : ApiResult<T>()
+    object SuccessNoContent : ApiResult<Nothing>()
+    data class Failure(val throwable: Throwable) : ApiResult<Nothing>()
+    object Loading : ApiResult<Nothing>()
+    object Loaded : ApiResult<Nothing>()
 }
